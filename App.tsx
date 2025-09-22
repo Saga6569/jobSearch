b0c09e74133d791a3eddc0e900f3786f3e6627b2
saga6569 <aristov6569@gmail.com>
2025-09-22 11:15:14 +0300

    add navigator

--- App.tsx
+++ App.tsx
@@ -2,11 +2,18 @@ import './global.css';
 // simport { SafeAreaProvider } from 'react-native-safe-area-context';
 import Main from './src/pages/Main';
 
+import * as React from 'react';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: Main,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
 export default function App() {
-  return (
-    <>
-      {/* <SafeAreaProvider></SafeAreaProvider> */}
-      <Main />
-    </>
-  );
+  return <Navigation />;
 }
